b5ac3a838181b9747f84548788ba7fcf3b17852d
João Faria <joao.faria@astro.up.pt>
2018-07-18 19:36:48 +0100

    add package and keplerian

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='trillian',
+      version='0.1',
+      description='Simulate RVs',
+      author='João Faria',
+      author_email='joao.faria@astro.up.pt',
+      license='GPLv3+',
+      url='https://github.com/j-faria/trillian',
+      packages=['trillian',],
+      intall_requires=['numpy',],
+     )
